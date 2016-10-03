d141691cd9151f5e0038bab60f856260d5740c94
iftruereturn <qwertyzxc11@gmail.com>
2016-10-03 10:40:24 +0300

    Add selection sort

---
+++ algorithms/sorting/selection-sort.js
@@ -0,0 +1,44 @@
+function swap(items, left, right) {
+  if (left !== right) {
+    let temp = items[left];
+    items[left] = items[right];
+    items[right] = temp;
+  }
+}
+
+function findIndexOfSmallestFromIndex(items, sortedRangeEnd) {
+  let currentSmallest = items[sortedRangeEnd];
+  let currentSmallestIndex = sortedRangeEnd;
+
+  for (let i = sortedRangeEnd + 1; i < items.length; i++) {
+    if (currentSmallest - items[i] > 0) {
+      currentSmallest = items[i];
+      currentSmallestIndex = i;
+    }
+  }
+
+  return currentSmallestIndex;
+}
+
+function selectionSort(items) {
+  let sortedRangeEnd = 0;
+
+  while (sortedRangeEnd < items.length) {
+    let nextIndex = findIndexOfSmallestFromIndex(items, sortedRangeEnd);
+    swap(items, sortedRangeEnd, nextIndex);
+
+    sortedRangeEnd++;
+  }
+}
+
+module.exports = selectionSort;
+
+//---------------------------------------------------------
+// example
+
+
+let arr = [8, 1, 4, 5, 2, 3, 7, 6, 0, 9];
+console.log(arr);
+selectionSort(arr);
+console.log(arr);
+
